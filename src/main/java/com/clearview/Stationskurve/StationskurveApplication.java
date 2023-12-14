b1fc2fbf3ce91b5931d82f6c0023cfc50c400668
veon <veon@posteo.de>
2023-12-14 12:51:36 +0100

    initial commit

---
+++ src/main/java/com/clearview/Stationskurve/StationskurveApplication.java
@@ -0,0 +1,13 @@
+package com.clearview.Stationskurve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationskurveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationskurveApplication.class, args);
+	}
+
+}

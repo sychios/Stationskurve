8c2135243a051dfdf461f93f47929495e97106ed
veon <veon@posteo.de>
2023-12-18 13:08:10 +0100

    Add sample htmx-insertion

--- src/main/java/com/clearview/stationskurve/StationskurveApplication.java
+++ src/main/java/com/clearview/stationskurve/StationskurveApplication.java
@@ -3,12 +3,21 @@ package com.clearview.stationskurve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class StationskurveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StationskurveApplication.class, args);
 	}
 
+
+	@PostMapping("/greet")
+	public String greet(){
+		return "HTMX is working.";
+	}
 }

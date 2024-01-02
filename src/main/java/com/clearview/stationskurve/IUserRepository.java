61864d7d34a58d1610623a9f09deb72811bf13b3
veon <veon@posteo.de>
2024-01-02 14:59:29 +0100

    Remove annotation to setup db connection

--- src/main/java/com/clearview/stationskurve/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clearview.stationskurve;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.clearview.stationskurve.User;
-
-// Crud refers to Create/Read/Update/Delete
-public interface IUserRepository  extends CrudRepository<User, Integer> {
-}
--- src/main/java/com/clearview/stationskurve/StationskurveApplication.java
+++ src/main/java/com/clearview/stationskurve/StationskurveApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 @RestController
 public class StationskurveApplication {
 

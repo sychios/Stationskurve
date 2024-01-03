7f2ffe0be9412e890e9fc642e20eff68f1d002d5
veon <veon@posteo.de>
2024-01-03 15:57:08 +0100

    Replaced variable annotations with class annotation for lombok

--- src/main/java/com/clearview/stationskurve/patient/Patient.java
+++ src/main/java/com/clearview/stationskurve/patient/Patient.java
@@ -2,32 +2,25 @@ package com.clearview.stationskurve.patient;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Patient {
-    @Setter
     @Id
     private Integer id;
 
-    @Setter
     private String name;
 
-    @Setter
     private String vorname;
 
-    @Setter
     private java.sql.Date geburtstag;
 
-    @Setter
     private Integer zimmer;
 
-    @Setter
     private java.sql.Timestamp aufnahme;
 
-    @Setter
     private boolean septisch;
 }
